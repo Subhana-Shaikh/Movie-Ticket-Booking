9af720f133686f0a4f663756fd03ed52eb54c8cf
Subhana Shaikh <shaikhsubhana2727@gmail.com>
2026-09-07 11:20:17 +0530

    v2.0-added latest screening movies

--- MTB.py
+++ MTB.py
@@ -1,5 +1,8 @@
-# Movie Ticket Booking System - Version 1.0
+# Movie Ticket Booking System - Version 2.0
 #Feature : Book and Cancel Tickets
+
+catalogue = ["Project Hail Mary", "Spider-Man Brand New Day","The Odyssey","Obsession"]
+
 def book_ticket(ticket_id, user_id) :
   print("Ticket", ticket_id, "Confirmed Ticket of", user_id)
 def cancel_ticket(ticket_id) :
@@ -9,3 +12,9 @@ def calculate_refund(days_late, rate = 5):
   refund= days_late*rate
   print("Refund=Rs.", refund)
   return refund
+
+def search_movie(title):
+    if title in catalogue:
+      print("Movie is Available")
+    else:
+      print(title, "Not Found")
