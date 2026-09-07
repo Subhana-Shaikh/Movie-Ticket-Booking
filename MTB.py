5785ea5a766f3f6a1d3d38db5bc15dd0b8b8d5e3
Subhana Shaikh <shaikhsubhana2727@gmail.com>
2026-09-07 11:10:58 +0530

    v1.1-added refund calculation

--- MTB.py
+++ MTB.py
@@ -5,3 +5,7 @@ def book_ticket(ticket_id, user_id) :
 def cancel_ticket(ticket_id) :
   print("Ticket", ticket_id, "Cancelled")
      
+def calculate_refund(days_late, rate = 5):
+  refund= days_late*rate
+  print("Refund=Rs.", refund)
+  return refund
