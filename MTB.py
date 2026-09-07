8941fde4b4b8e05f7ecf12a86ab280fc79a91c74
Subhana Shaikh <shaikhsubhana2727@gmail.com>
2026-09-07 11:05:37 +0530

    v1.0- basic book and cancel ticket

---
+++ MTB.py
@@ -0,0 +1,7 @@
+# Movie Ticket Booking System - Version 1.0
+#Feature : Book and Cancel Tickets
+def book_ticket(ticket_id, user_id) :
+  print("Ticket", ticket_id, "Confirmed Ticket of", user_id)
+def cancel_ticket(ticket_id) :
+  print("Ticket", ticket_id, "Cancelled")
+     
